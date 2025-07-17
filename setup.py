908df9671dba3cc5e1fa724e92e3055912e79a4e
datta-abhi <rookbee91@gmail.com>
2025-07-17 10:35:37 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A small mlops project showing use of DVC, mlflow, model registry in an ML project',
+    author='Abhigyan',
+    license='',
+)
